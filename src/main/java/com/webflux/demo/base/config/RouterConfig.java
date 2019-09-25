262c6ea36071493baee12ce0bef6ab1b65693834
zhangxike <zhangxike@wondersgroup.com>
2019-09-25 16:28:37 +0800

    增加时间 endpoint

---
+++ src/main/java/com/webflux/demo/base/config/RouterConfig.java
@@ -0,0 +1,28 @@
+package com.webflux.demo.base.config;
+
+import com.webflux.demo.base.handler.TimeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+/**
+ * @program: webflux
+ * @description:
+ * @author: Zhangxike
+ * @create: 2019-09-25 16:09
+ **/
+@Configuration
+public class RouterConfig {
+    @Autowired
+    private TimeHandler timeHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> timeRouter(){
+        return route(GET("/time"),timeHandler::getTime).andRoute(GET("/date"),timeHandler::getDate);
+    }
+}
--- src/main/java/com/webflux/demo/base/controller/BaseController.java
+++ src/main/java/com/webflux/demo/base/controller/BaseController.java
@@ -1,4 +1,4 @@
-package com.webflux.demo.base;
+package com.webflux.demo.base.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
