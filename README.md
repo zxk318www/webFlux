445be8b9efce770fe18bc4e89bfbfcae28d2fd84
zhangxike <zhangxike@wondersgroup.com>
2019-09-25 15:59:29 +0800

    webflux 项目初始化

--- README.md
+++
@@ -1,2 +0,0 @@
-# webflux
-WebFlux
---
+++ src/main/java/com/webflux/demo/base/BaseController.java
@@ -0,0 +1,20 @@
+package com.webflux.demo.base;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @program: webflux
+ * @description:
+ * @author: Zhangxike
+ * @create: 2019-09-25 15:53
+ **/
+@RestController
+public class BaseController {
+
+    @GetMapping("/index")
+    public Mono<String> index(){
+        return Mono.just("This is a WebFlux project");
+    }
+}
